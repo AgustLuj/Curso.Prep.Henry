7ff87ec8439cdbcdffdba435c381ca292b9ecead
Agustin <0j30j2@gmail.com>
2020-07-07 20:18:19 -0300

    03-JS-LL

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -61,11 +61,11 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  if((numero % 3) === 0){
+  if((numero % 3) === 0 && ((numero % 5) != 0)){
     return 'fizz';
-  }else if ((numero % 5) === 0){
+  }else if ((numero % 5) === 0 && ((numero % 3) != 0)){
     return 'buzz';
-  }else if (((numero % 3) === 0) && ((numero % 5) === 0)){
+  }else if (((numero % 3) == 0) && ((numero % 5) == 0)){
     return 'fizzbuzz';
   }else{
     return numero;
@@ -78,7 +78,22 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  return
+  let primo = true;
+  if(numero<2)
+  {
+      primo = false;
+  }
+  else
+  {
+      for(let x=2; x*x<=numero; x++)
+      {
+          if( numero%x==0 ){
+              primo = false;
+              break;
+          }
+      }
+  }
+  return primo;
 }
 
 
